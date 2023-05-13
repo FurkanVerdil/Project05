935cf046e965ad9d52aa8d765e82c77632a8dc1b
FurkanVerdil <furkanverdil0@gmail.com>
2023-05-13 21:04:05 +0300

    Project-05

---
+++ orn4/orn4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orn4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        //Kullanıcının girmiş olduğu sayının tam bölen adedini gösteren uygulamayı yazınız.c#
+
+            Console.WriteLine("Girilen sayı: ");
+            int sayi =int.Parse(Console.ReadLine());
+
+            int adet = 0;
+            int f = 1;
+
+            while (sayi <= f)
+            {
+                if (sayi % f==0)
+                {
+
+                   adet++;
+                    
+                }
+                f++;
+            }
+            Console.WriteLine( "tam bölen: " +adet );
+            Console.ReadKey();
+        }
+    }
+}
